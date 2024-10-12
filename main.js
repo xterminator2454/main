fde1c38fccdb9ec1e24387426298c66198a83492
xterminator2454 <112624573+xterminator2454@users.noreply.github.com>
2024-10-12 16:45:46 +0530

    Update main.js

--- main.js
+++ main.js
@@ -15,7 +15,7 @@ app.use(express.static('public'));
 
 // Serve HTML form for file upload
 app.get('/', (req, res) => {
-    res.sendFile(path.join(__dirname, 'public', 'index.html'));
+    res.sendFile(path.join(__dirname, 'index.html'));
 });
 
 // Upload endpoint
